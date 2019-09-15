4eb5060e0842fbb5a8b2e6466634a2d00fa39057
903875816 <604150318@qq.com>
2019-09-15 21:06:13 +0800

    求1到n之间有多少个质数（素数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -3,6 +3,7 @@ package com.github.hcsp.controlflow;
 public class Main {
     public static void main(String[] args) {
         System.out.println(howManyPrimeNumbers(100));
+        System.out.println(howManyPrimeNumbers(5));
     }
 
     /**
@@ -20,15 +21,15 @@ public class Main {
         if (n == 1 || n == 2||n<=0) {
             return 0;
         } else {
-            for(int i=3;i<=n;i++) {
-                for (int j = 2; j <= (int) (Math.sqrt(i) + 1); j++) {
-                    if (i % j == 0) {
+            for(int i=3;i<n;i++) {//遍历n之内的2-n的整数
+                for (int j = 2; j <= (int) (Math.sqrt(i) + 1); j++) {//判断这个整数是不是质数
+                    if (i % j == 0 ) {//可以除尽说明这个数不是质数，跳出循环，不是质数的数加1
                         count++;
-                        break;
+                        break;//这数不是质数，跳出内层for循环
                     }
                 }
             }
-            return n-count-1;
+            return n-count-2;//总的数减不是质数的数减本身减1就是质数的数量
         }
     }
 }
