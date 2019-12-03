802a9a402f2fc283c10206cce25487af72e898e3
MacZhy <809775352@qq.com>
2019-12-03 17:38:56 +0800

    Java的控制流练习：求1到n之间有多少个质数（素数）fefefe

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -17,21 +17,21 @@ public class Main {
      */
     public static int howManyPrimeNumbers(int n) {
         int num = 0;
-
+        int j;
         boolean sgin;
         for (int i = 2; i <= n; i++) {
-            if(i % 2 == 0 && i != 2  )  {
-                continue; //偶数和1排除
 
-            }
-            sgin= true;
-            for (int j = 2; j <= Math.sqrt(i) ; j++) {
+            if(i % 2 == 0 && i != 2  )  continue; //偶数和1排除
+
+            sgin = true;
+            for (j = 2; j <= i/2 ; j++) {
                 if (i % j == 0) {
-                    sgin = false;
+                    sgin= false;
                     break;
                 }
             }
 
+            //打印
             if (sgin) {
                 num++;
             }
