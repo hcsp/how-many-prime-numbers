db3131b6e8cba168252b2e1e78567428b8f65727
MacZhy <809775352@qq.com>
2019-12-03 17:35:53 +0800

    Java的控制流练习：求1到n之间有多少个质数（素数）

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,27 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int num = 0;
+
+        boolean sgin;
+        for (int i = 2; i <= n; i++) {
+            if(i % 2 == 0 && i != 2  )  {
+                continue; //偶数和1排除
+
+            }
+            sgin= true;
+            for (int j = 2; j <= Math.sqrt(i) ; j++) {
+                if (i % j == 0) {
+                    sgin = false;
+                    break;
+                }
+            }
+
+            if (sgin) {
+                num++;
+            }
+        }
+        return num;
+    }
 }
