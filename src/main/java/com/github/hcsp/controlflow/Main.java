cd414d802e92e3eb515e4b073042bb6a35bc8ff5
lazzben <458861463@qq.com>
2020-05-27 18:45:36 +0800

    求1到n之间有多少个质数（素数）

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,14 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        if (n == 1) return 0;
+        int count = 0;
+        for (int i = 2; i <= Math.sqrt(n) + 1; i++) {
+            if (n % i != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
