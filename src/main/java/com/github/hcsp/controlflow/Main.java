3bd68f8ef1ca454c7261a07520010422eb1cd212
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-04-21 21:49:27 +0800

    1- n内的质数个数(参考模范代码，部分修改自己的代码）

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -35,12 +35,12 @@ public class Main {
         return count;
     }
 
-    private static boolean isPrime(int n) {
-        for (int i = 2; i < 1 + Math.sqrt(n); i++) {
-            if (n % i == 0 && n != 2) {
-                return false;
-            }
-        }
-        return true;
-    }
+//    private static boolean isPrime(int n) {
+//        for (int i = 2; i < 1 + Math.sqrt(n); i++) {
+//            if (n % i == 0 && n != 2) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 }
