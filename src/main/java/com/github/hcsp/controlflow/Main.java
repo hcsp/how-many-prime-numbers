63e65b1db0843c0766c20e92b6976267864f2e8a
DEMAO <987854018@qq.com>
2021-09-28 16:05:11 +0800

    求1到n之间有多少个质数（素数）

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -2,7 +2,7 @@ package com.github.hcsp.controlflow;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(howManyPrimeNumbers(100));
+        System.out.println(howManyPrimeNumbers(100));  //1234
     }
 
     /**
@@ -10,10 +10,27 @@ public class Main {
      *
      * <p>例如，n=5，返回2，因为1到5之间有2个质数：2和3。
      *
-     * <p>提示：对于正整数n，如果用2到Math.sqrt(n)+1之间的所有整数去除，均无法整除，则n为质数。
+     * <p>提示：对于正整数n，如果用2到Math.sqrt(n)+1之间的所有整数去除，均无法整除，则n为质数。 // x /2 ... Math.sqrt(n)+1
      *
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        boolean zhishu = true;
+        int geshu = 1;
+        for (int x = 3; x < n; x++) {
+            for (int i = 2; i < Math.sqrt(x)+1; i++ ) {
+                if ((x % i) == 0) {   //3
+                    zhishu = false;
+                    break;//如果是合数 false
+                } else {
+                    zhishu = true;// i ++
+                }
+            }     //jieguo 要不就是false yaobu jiushi true
+            if (zhishu == true) {
+                geshu++;
+            }
+        }
+        return geshu;
+    }
 }
